6480648df5cb98cf24d2c05c6a723ca16d05c035
daiyang <dystargate@gmail.com>
2019-07-11 12:04:37 +0800

    Add the router_controllers file

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,55 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["beego-app/controllers:StudentController"] = append(beego.GlobalControllerRouter["beego-app/controllers:StudentController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["beego-app/controllers:StudentController"] = append(beego.GlobalControllerRouter["beego-app/controllers:StudentController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["beego-app/controllers:StudentController"] = append(beego.GlobalControllerRouter["beego-app/controllers:StudentController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["beego-app/controllers:StudentController"] = append(beego.GlobalControllerRouter["beego-app/controllers:StudentController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["beego-app/controllers:StudentController"] = append(beego.GlobalControllerRouter["beego-app/controllers:StudentController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
