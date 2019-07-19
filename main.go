0ebb0e20bfd84c68c89b1a02c8ca7a74b0b786bb
daiyang <dystargate@gmail.com>
2019-07-19 15:31:37 +0800

    Add the beego plugins to enable basic auth support

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "beego-app/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/auth"
 )
 
 func main() {
@@ -11,5 +12,15 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// Package auth provides handlers to enable basic auth support
+	authPlugin := auth.NewBasicAuthenticator(SecretAuth, "Authorization Required")
+	beego.InsertFilter("*", beego.BeforeRouter, authPlugin)
+
 	beego.Run()
+}
+
+func SecretAuth(username, password string) bool {
+	// your own basic auth logic
+	return username == "test" && password == "123456"
 }
